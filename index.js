db16dad229e8fd64aaef3f110b14c1531f449fd8
raframos@deloitte.com-at-766691614904 <raframos@deloitte.com-at-766691614904>
2021-05-04 15:40:42 -0300

    inicial

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+const categoriesController = require('./categories/categoriesController');
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use("/", categoriesController);
+
+app.listen(8081, () => {
+    console.log('O servidor esta rodando')
+});
